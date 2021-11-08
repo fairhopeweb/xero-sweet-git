88db02ab2ccaf9849a6650f2164dae4ff7c612f2
TechXero <steve@techxero.com>
2021-11-08 14:19:44 +0200

    Initial

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#set -e
+echo "##########################################"
+echo "Be Careful this will override your Rice!! "
+echo "##########################################"
+sleep 5
+echo "Creating Backups of ~/.config folder"
+echo "#####################################"
+cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
+sleep 2
+echo "Installing required Tools"
+echo "#################################"
+sudo pacman -S --needed --noconfirm kvantum-qt5 latte-dock lolcat neofetch yakuake
+sleep 2
+echo "Installing Sweet Theme"
+echo "#################################"
+sudo pacman -S --needed --noconfirm sweet-gtk-theme-dark sweet-kde-git kvantum-theme-sweet-git plasma-theme-sweet beautyline
+sleep 2
+echo "Installing Fonts"
+echo "#################################"
+sudo pacman -S --needed --noconfirm nerd-fonts-hack nerd-fonts-fira-code nerd-fonts-meslo nerd-fonts-terminus noto-fonts-emoji
+sleep 2
+echo "Applying new Rice, hold on..."
+echo "#################################"
+cp -Rf Configs/Home/. ~
+sudo cp -Rf Configs/System/. /
+sudo sed -i "s/Current=breeze/Current=Shiny-SDDM/g" /etc/sddm.conf
+sleep 2
+echo "Applying Grub Theme...."
+echo "#################################"
+chmod +x CyberRe.sh
+sudo ./CyberRe.sh
+sleep 2
+echo "Rebooting system in 5 seconds..."
+echo "#################################"
+sleep 5
+sudo reboot
